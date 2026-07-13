84c2c728c46c2def1eaab19939910cc89d08fbb8
LEEHY\ahens <ahemswl9806@naver.com>
2026-07-13 13:32:55 +0900

    feat: MSA 멀티모듈 프로젝트 구조 설정 및 서비스 분리

--- cart-service/src/main/java/com/sparta/msa/cart/CartServiceApplication.java
+++ cart-service/src/main/java/com/sparta/msa/cart/CartServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.sparta.msa.cart", "com.sparta.msa.common"})
 @EnableFeignClients
 public class CartServiceApplication {
     public static void main(String[] args) {
--- user-service/src/main/java/com/sparta/msa/user/UserServiceApplication.java
+++ user-service/src/main/java/com/sparta/msa/user/UserServiceApplication.java
@@ -3,7 +3,7 @@ package com.sparta.msa.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.sparta.msa.user", "com.sparta.msa.common"})
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
